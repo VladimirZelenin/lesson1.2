b461d4debf98d08d5b69f54f8961e804ab37e571
VladimirZelenin <zeleninvlv@gmail.com>
2024-08-15 20:02:00 +0300

    Add files via upload
    Практическое задание по теме. Переменные

---
+++ lesson1.2.py
@@ -0,0 +1,6 @@
+number_of_completed_homework = "12"
+number_of_hours_spent = "1.5"
+name_of_course = "Python"
+time_for_one_task = "float(number_of_hours_spent)/float(number_of_completed_homework)"
+print ('Курс:',name_of_course,',', 'всего задач:',number_of_completed_homework,',','затрачено часов:',number_of_hours_spent,',','среднее время выполнения:',float(number_of_hours_spent)/float(number_of_completed_homework), 'часа','.')
+ 
